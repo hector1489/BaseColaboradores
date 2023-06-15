c51c30c9e62fb39d27879367edae5ce76900ee08
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2023-06-15 19:12:13 -0400

    request6 (browser)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
-import Browser from './components/Browser/Browser'
-import Table from './components/Table/Table'
-import Form from './components/Form/Form'
-import { BaseColaboradores } from './BaseColaboradores'
+import React, { useState } from 'react';
+import Browser from './components/Browser/Browser';
+import Table from './components/Table/Table';
+import Form from './components/Form/Form';
+import { BaseColaboradores } from './BaseColaboradores';
 
 function App() {
   const [colaboradores, setColaboradores] = useState(BaseColaboradores);
+  const [terminoBusqueda, setTerminoBusqueda] = useState('');
 
   const agregarColaborador = (nuevoColaborador) => {
     nuevoColaborador.id = +colaboradores[colaboradores.length -1].id +1
@@ -17,12 +18,25 @@ function App() {
     setColaboradores(nuevosColaboradores);
   };
 
+  const handleTerminoBusqueda = (e) => {
+    setTerminoBusqueda(e.target.value);
+  };
+
+  const filteredColaboradores = colaboradores.filter(
+    (colaborador) =>
+      colaborador.nombre.toLowerCase().includes(terminoBusqueda.toLowerCase()) ||
+      colaborador.correo.toLowerCase().includes(terminoBusqueda.toLowerCase()) ||
+      colaborador.edad.toString().includes(terminoBusqueda.toLowerCase()) ||
+      colaborador.cargo.toLowerCase().includes(terminoBusqueda.toLowerCase()) ||
+      colaborador.telefono.toLowerCase().includes(terminoBusqueda.toLowerCase())
+  );
+
   return (
     <div className="container">
-      <Browser />
+       <Browser handleTerminoBusqueda={handleTerminoBusqueda} />
       <div className="row">
         <div className="col-sm-8">
-          <Table colaboradores={colaboradores} eliminarColaborador={eliminarColaborador} />
+          <Table colaboradores={filteredColaboradores} eliminarColaborador={eliminarColaborador} />
         </div>
         <div className="col-sm-4">
           <Form agregarColaborador={agregarColaborador} />
@@ -32,4 +46,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
--- src/components/Browser/Browser.jsx
+++ src/components/Browser/Browser.jsx
@@ -1,10 +1,15 @@
-const Browser = () => {
-    return (
-        <div className="container">
-        <h1>Listado de colaboradores</h1>
-        <input type="text" />
-        </div>
-    );
-}
+
+const Browser = ({ handleTerminoBusqueda }) => {
+  const handleChange = (e) => {
+    handleTerminoBusqueda(e.target.value);
+  };
+
+  return (
+    <div className="container">
+      <h1>Listado de colaboradores</h1>
+      <input type="text" onChange={handleChange} />
+    </div>
+  );
+};
 
 export default Browser
