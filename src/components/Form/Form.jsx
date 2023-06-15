16611e0d993b6122791ec8832a5f0a10c6396080
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2023-06-15 14:44:13 -0400

    request(form)

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,63 +1,65 @@
-import React, { useState } from 'react'
-import Alert from '../Alert/Alert'
+import React, { useState } from 'react';
+import Alert from '../Alert/Alert';
 
 const Form = () => {
-  const [pass, setPass] = useState('');
-  const [confirmPass, setConfirmPass] = useState('');
-  const [showAlert, setShowAlert] = useState(false)
-  const [email, setEmail] = useState('');
+  const [colaboradores, setColaboradores] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
 
-  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+  const agregarColaborador = (nuevoColaborador) => {
+    setColaboradores([...colaboradores, nuevoColaborador]);
+    setShowAlert(true);
+  };
 
-  const handleConfirmPassword = () => {
-    if (pass === confirmPass) {
-      setShowAlert(true);
+  const handlerSend = (e) => {
+    e.preventDefault();
+    const nombre = e.target.elements.nombre.value;
+    const correo = e.target.elements.correo.value;
+    const edad = e.target.elements.edad.value;
+    const cargo = e.target.elements.cargo.value;
+    const telefono = e.target.elements.telefono.value;
+
+    if (nombre && correo && edad && cargo && telefono) {
+      const nuevoColaborador = {
+        id: colaboradores.length + 1,
+        nombre,
+        correo,
+        edad,
+        cargo,
+        telefono
+      };
+      agregarColaborador(nuevoColaborador);
+      e.target.reset();
     } else {
       setShowAlert(false);
     }
-    handleConfirmEmail();
   };
-
-  const handleConfirmEmail = () => {
-    if (emailRegex.test(email) === false) {
-      alert('Ingresa un correo válido');
-    }
-  }
-
-  const handlerSend = (e) => {
-    handleConfirmPassword();
-    e.preventDefault();
-  }
 
   return (
     <form onSubmit={handlerSend}>
       <h4>Agregar colaborador</h4>
       <div className="form-group">
-        <label htmlFor="exampleInputName"/>
-        <input type="name" className="form-control" id="exampleInputName" placeholder="Nombre del Colaborador" />
+        <label htmlFor="exampleInputName">Nombre del Colaborador</label>
+        <input type="text" className="form-control" id="exampleInputName" name="nombre" placeholder="Nombre del Colaborador" required />
       </div>
       <div className="form-group">
-        <label htmlFor="exampleInputEmail1"/>
-        <input type="email" className="form-control" id="exampleInputEmail" placeholder="Email del colaborador" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <label htmlFor="exampleInputEmail">Email del colaborador</label>
+        <input type="email" className="form-control" id="exampleInputEmail" name="correo" placeholder="Email del colaborador" required />
       </div>
       <div className="form-group">
-        <label htmlFor="exampleInputPassword1"/>
-        <input type="number" className="form-control" id="exampleInputPassword1" placeholder="Edad" value={pass} onChange={(e) => setPass(e.target.value)} />
+        <label htmlFor="exampleInputPassword1">Edad</label>
+        <input type="number" className="form-control" id="exampleInputPassword1" name="edad" placeholder="Edad" required />
       </div>
       <div className="form-group">
-        <label htmlFor="exampleInputPassword2"/>
-        <input type="text" className="form-control" id="exampleInputPassword2" placeholder="Cargo" value={confirmPass} onChange={(e) => setConfirmPass(e.target.value)} />
+        <label htmlFor="exampleInputPassword2">Cargo</label>
+        <input type="text" className="form-control" id="exampleInputPassword2" name="cargo" placeholder="Cargo" required />
       </div>
       <div className="form-group">
-        <label htmlFor="exampleInputPassword2"/>
-        <input type="number" className="form-control" id="exampleInputPassword2" placeholder="Telofono del colaborador" value={confirmPass} onChange={(e) => setConfirmPass(e.target.value)} />
+        <label htmlFor="exampleInputPassword3">Teléfono del colaborador</label>
+        <input type="tel" className="form-control" id="exampleInputPassword3" name="telefono" placeholder="Teléfono del colaborador" required />
       </div>
       <button type="submit" className="btn btn-success">Agregar Colaborador</button>
       {showAlert && (
         <Alert texto="¡Nuevo colaborador agregado!" color="success" />
-      )}
-      {!showAlert && (
-        <Alert texto="¡Completa los campos!" color="danger" />
       )}
     </form>
   );
